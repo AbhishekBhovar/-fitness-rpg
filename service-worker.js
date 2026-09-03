541c0a408a40ff64606bb56cbc7fbd20cf97b46a
AbhishekBhovar <abhishekbhovar@gmail.com>
2026-09-04 00:11:25 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE='level-up-blue-glossary-v20260903-1';
+const CACHE='level-up-fitness-v20260904-balanced-1';
+const PREFIX='level-up-fitness-';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(PREFIX)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
