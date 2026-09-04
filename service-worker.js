8018b38dd6e15db883cee17c074d070ec6a2225f
AbhishekBhovar <abhishekbhovar@gmail.com>
2026-09-04 20:37:41 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='level-up-fitness-v45-final-character-logo-patch-20260904';
+const CACHE='level-up-fitness-v46-assessment-ranking-fix-20260904';
 const PREFIX='level-up-fitness-';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./assets/level_up_logo_final_v45.png'])))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(PREFIX)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
